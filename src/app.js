ff6f9988307c0689b25b831acfe2e3f1dfa6c5a9
ikramhasib007 <ikramhasib007@gmail.com>
2018-05-01 02:20:17 +0600

    water bill order changes

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
 const store = configureStore();
-store.dispatch(addExpense({description: 'Water Bill', amount: 500, createdAt:moment().subtract(15,'d'), note:'Paid through Agrani Bank. Rampura Branch, Dhaka, Bangladeh.'}));
-store.dispatch(addExpense({description: 'Gas Bill', amount: 800, createdAt:moment().subtract(10,'d'), note:'Paid through Agrani Bank. Badda Branch, Dhaka, Bangladeh.'}));
+store.dispatch(addExpense({description: 'Gas Bill', amount: 800, createdAt:moment().subtract(15,'d'), note:'Paid through Agrani Bank. Badda Branch, Dhaka, Bangladeh.'}));
+store.dispatch(addExpense({description: 'Water Bill', amount: 500, createdAt:moment().subtract(10,'d'), note:'Paid through Agrani Bank. Rampura Branch, Dhaka, Bangladeh.'}));
 store.dispatch(addExpense({description: 'House Rent', amount: 10000, createdAt:moment().subtract(2,'d'), note:'Cash send by bKash. Nearest bKash agent. Dhaka, Bangladesh'}));
 store.dispatch(addExpense({description: 'Utility Rent', amount: 1200, createdAt:moment(), note:'Cash send by bKash. Nearest bKash agent. Dhaka, Bangladesh'}));
 
